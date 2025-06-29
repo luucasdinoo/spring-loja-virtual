2be4213ac05282f1eedafea552d6723d36f17bb9
Lucas Bernadino Silvestre da Silva <luucasdinoo@gmail.com>
2025-06-29 15:08:33 -0300

    Commit inicial projeto loja virtual

---
+++ src/main/java/com/dino/lojavirtual/SpringLojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.dino.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLojaVirtualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLojaVirtualApplication.class, args);
+    }
+
+}
